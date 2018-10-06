58338fdf610436e95038acfd9c1b2a59f8a9a015
wenyacheng <872453942@qq.com>
2018-10-07 00:37:03 +0800

    meau点击动画效果初步实现

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="box">
+    <div class="box"  :class="{'layout-hide-text': spanLeft < 5}">
        <i-col :span="spanLeft" class="layout-menu-left">
-        <Menu theme="dark" class="navList" accordion >
+        <Menu theme="dark" class="navList" accordion :span="spanLeft">
         <!-- 左边内容 -->
         <div class="layout-logo-left">
                 <span class="layout-text"> Admin 管理系统</span>
@@ -14,7 +14,7 @@
                         <span class="layout-text" >{{item.name}}</span>
                     </template>
                     <template v-for="(child,childIndex) in item.children" v-if="!child.hidden">
-                        <Menu-item :name="child.path" :key="child.id"><router-link :to="child.path">{{child.name}}</router-link></Menu-item>
+                        <Menu-item :name="child.path" :key="child.id" ><router-link :to="child.path">{{child.name}}</router-link></Menu-item>
                     </template>
                 </Submenu>
                 
@@ -23,6 +23,11 @@
      </i-col>
         <!-- 右边内容 -->
      <i-col :span="spanRight">
+         <div class="layout-header">
+                    <i-button type="text" @click="toggleClick">
+                        <Icon type="md-menu"  size="32"/>
+                    </i-button>
+                </div>
                 <!-- 中间内容 -->
                 <div class="layout-content">
                     <div class="layout-content-main">
@@ -36,22 +41,28 @@
 
 <script>
 
-    export default{
-         name:'navList',
-        data(){
-            return{
-                spanLeft: 5,//左边内容的宽度
-                spanRight: 19//中间内容的宽度
+   export default {
+        data () {
+            return {
+                spanLeft: 5,
+                spanRight: 19,
             }
         },
         computed: {
-            iconSize () {//字体大小设置
+            iconSize () {
                 return this.spanLeft === 5 ? 14 : 24;
             }
         },
         methods: {
-            //路由点击实现内容显示
-            
+            toggleClick () {
+                if (this.spanLeft === 5) {
+                    this.spanLeft = 2;
+                    this.spanRight = 22;
+                } else {
+                    this.spanLeft = 5;
+                    this.spanRight = 19;
+                }
+            }
           
         }
     }
@@ -76,17 +87,28 @@
     }
     .layout-menu-left{
         background: #515a6e;
-        width: 240px;
         height: 100%;
         position: relative;
        
         .navList{
             position: absolute;
-            top: 20px
+            top: 20px;
+            background: #515a6e;
+            width: auto !important;
+
+            a{
+                color:#fff !important
+            }
         }
         .layout-text{
             color: #fff;
         }
         
+    }
+    .ivu-col{
+        transition: width .2s ease-in-out;
+    }
+    .layout-hide-text .layout-text{
+        display: none;
     }
 </style>
